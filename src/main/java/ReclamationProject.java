9fa7cbe91dbac89c90d145fd62ba3329be00a447
Noah Igoe <Noah Igoe@Camille>
2017-09-27 16:08:18 -0500

    I made it pretty, commented sections and changed variable names.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -7,20 +7,48 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+public class ReclamationProject {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+    /**
+     * The main that can be used to text out the largestMatch method.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+        System.out.println(largestMatch("123abcdef", "abcdefghijk"));
+    }
+
+     /**
+      * A method that takes two strings as an input and returns the
+      * largest substring that the two strings have in common.
+      * @param str1 a String taken as the input
+      * @param str2 a String taken as the input
+      * @return match the largest substring that the two Strings have in common
+      */
+    static String largestMatch(final String str1, final String str2) {
+        String smaller = str1; //always shorter
+        String larger = str2; //always longer
+        String match = ""; //the largest matching section between the two strings
+
+        //makes sure that larger is longer length than smaller
+        if (smaller.length() > larger.length()) {
+            String temp = str1;
+            smaller = str2;
+            larger = temp;
+        }
+
+        //goes through the all comparisons of two words
+        for (int i = 0; i < smaller.length(); i++) {
+            for (int j = smaller.length() - i; j > 0; j--) {
+                for (int k = 0; k < larger.length() - j; k++) {
+                    //checks if there is a substring that matches between the Strings
+                    //checks the substring that matches is greater than the last match
+                    //assigns greatest matching substring to match
+                    if (smaller.regionMatches(i, larger, k, j) && j > match.length()) {
+                        match = smaller.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return match;
+    }
 }
